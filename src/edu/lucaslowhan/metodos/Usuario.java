19b2ac8bffb2a7927dc7b54621cc5b4b381155dd
Lucas Lowhan <lucas.lowhan@hotmail.com>
2024-06-28 08:36:11 -0300

    Commit aula metodos.

--- src/edu/lucaslowhan/metodos/Usuario.java
+++ src/edu/lucaslowhan/metodos/Usuario.java
@@ -1,3 +1,13 @@
+/* <h1>SmartTV</h1>
+* A SmartTV simula um SmartTV real, onde criamos um objeto chamado smartTV e podemos alterar
+* canais, alterar volumes ligar e desligar.<p>
+* Inicialmente tem um loop onde pergunta se o usuario deseja ligar ou não a TV.
+* Caso ele escolha ligar a TV irá para um novo loop de comandos onde ele pode alterar canais,volume e também desligar a TV.
+* Caso ele escolha uma opção incorreta, irá retornar o loop e pedir para ele escolher a opção correta.
+*
+* @author Lucas Lowhan
+*/
+
 package edu.lucaslowhan.metodos;
 import java.util.Scanner;
 
@@ -5,18 +15,18 @@ public class Usuario {
     public static void main (String[] args){
         Scanner s = new Scanner(System.in);
         SmartTV smartTV = new SmartTV();
-        int ligar,opcao,canal;
-        System.out.println("Deseja ligar a tv? \n 1-Ligar \n 2-Não");
+        int ligar = 0,opcao,canal;
+        while (ligar != 1 && ligar !=2) {
+            System.out.println("Deseja ligar a tv? \n 1-Ligar \n 2-Não");
             ligar = s.nextInt();
-                if (ligar == 1){
-                    smartTV.ligar();
-                }
-                else if (ligar == 2){
-                    System.out.println("Tv desligada!");
-                }
-                else {
-                    System.out.println("Opção incorreta!");
-                }
+            if (ligar == 1) {
+                smartTV.ligar();
+            } else if (ligar == 2) {
+                System.out.println("Tv desligada!");
+            } else {
+                System.out.println("Opção incorreta!");
+            }
+        }
 
         while (smartTV.ligada == true){
             System.out.println("Escolha uma das opções: \n \n 1-Aumentar canal \n 2-Diminuir canal \n 3-Mudar canal");
@@ -46,12 +56,6 @@ public class Usuario {
                         System.out.println("Opção invalida, digite novamente");
                     }
         }
-
-        /*
-        smartTV.ligar();
-        smartTV.aumentarVolume();
-        smartTV.aumentarCanal();
-        smartTV.mudarCanal(25);*/
     }
 
 }
